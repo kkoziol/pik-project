8799232588ecb287584cb9f47a87a3a360877110
GPR-LENOVO-\gpr <gprzytula95@gmail.com>
2017-05-13 17:46:54 +0200

    init commit

---
+++ EbayView/src/main/java/com/project/pik/EbayView/EbayViewApplication.java
@@ -0,0 +1,32 @@
+package com.project.pik.EbayView;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import com.project.pik.EbayView.services.UserService;
+import com.project.pik.EbayView.services.UserServiceImpl;
+
+@Configuration
+@ComponentScan("com")
+@EnableAutoConfiguration
+public class EbayViewApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(EbayViewApplication.class, args);
+	}
+
+	@Bean
+	public EmbeddedServletContainerCustomizer containerCustomizer() {
+		return container -> container.setPort(8080);
+	}
+	
+	
+	@Bean UserService userService() {
+		return new UserServiceImpl();
+	}
+
+}
