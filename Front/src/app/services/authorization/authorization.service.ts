1f4b031936b3206d14c5501b3441a4faa2cc13b2
pirzmak <pirzmul@interia.eu>
2017-05-22 23:58:56 +0200

    Change link after login/logout , styles

--- Front/src/app/services/authorization/authorization.service.ts
+++ Front/src/app/services/authorization/authorization.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Authorize} from "./authorization.model";
-import {Observable} from "rxjs/Observable";
-import {Headers,Http, RequestOptions, URLSearchParams} from "@angular/http";
+import {Headers, Http, RequestOptions, URLSearchParams} from "@angular/http";
+import {ActivatedRoute, Router} from "@angular/router";
 
 
 @Injectable()
@@ -9,18 +9,18 @@ export class AuthorizationService {
   authorize: Authorize;
   token: string;
 
-  constructor(private http: Http) {
-    this.authorize = {active : false};
+  constructor(private http: Http, private router: Router) {
+    this.authorize = {active: false};
     const currentUser = JSON.parse(localStorage.getItem('currentUser'));
 
-    if(currentUser) {
+    if (currentUser) {
       this.token = currentUser.token;
       this.authorize.active = true;
     }
   }
 
   login(username: string, password: string) {
-    const url = 'https://localhost:8800/oauth/token';
+    const url = 'https://192.168.1.72:8800/oauth/token';
 
     const paramsT: URLSearchParams = new URLSearchParams('grant_type=password&username=' + username + '&password=' + password);
 
@@ -31,16 +31,19 @@ export class AuthorizationService {
     headers.append('Accept', 'application/json');
     headers.append('Content-Type', 'application/x-www-form-urlencoded');
 
-    const options = new RequestOptions({ headers: headers, params: paramsT});
+    const options = new RequestOptions({headers: headers, params: paramsT});
 
-    this.http.post(url, undefined , options)
-      .map(res =>  res.json())
-      .subscribe(access_token => this.token = access_token.access_token,
-                error2 => console.log("Zle haslo"));
+    this.http.post(url, undefined, options)
+      .map(res => res.json())
+      .subscribe(access_token => {
+          this.token = access_token.access_token;
+          this.authorize.active = true;
+          this.router.navigate(['./eBay']);
+        },
+        error2 => console.log("Zle haslo"));
 
-    localStorage.setItem('currentUser', JSON.stringify({ token: this.token}));
+    localStorage.setItem('currentUser', JSON.stringify({token: this.token}));
   }
-
 
 
 }
--- Front/src/app/services/eBayApi/eBayApi.service.ts
+++ Front/src/app/services/eBayApi/eBayApi.service.ts
@@ -15,7 +15,7 @@ export class EBayService {
   }
 
   getSbsCategoriesByParentId(parentId: number) {
-    return this.authorizationHttp.get("/categories/subcategories/" + parentId);
+    return this.authorizationHttp.get("/categories/subcategories/" + parentId).map(res =>  res.json());
   }
 
   getSpecificsCategoriesById(categoryId: number) {
@@ -31,11 +31,11 @@ export class EBayService {
   }
 
   getItemsByKeyWord(keyword: string) {
-    return this.authorizationHttp.get("/items/search/" + keyword);
+    return this.authorizationHttp.get("/items/search/" + keyword).map(res =>  res.json());
   }
 
-  getItemsByKeyWordAndCategory(keyword: string, categoryId: number) {
-    return this.authorizationHttp.get("/items/search/" + keyword + "/" + categoryId);
+  getItemsByKeyWordAndCategory(keyword: string, categoryId: string) {
+    return this.authorizationHttp.get("/items/search/" + keyword + "/" + categoryId).map(res =>  res.json());
   }
 
   getItemsByKeyWordAndCategoryAndMinMaxPrice(keyword: string, categoryId: number, minPrice: number, maxPrice: number) {
