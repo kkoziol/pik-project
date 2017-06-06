3655ed28574d14ce719b0a773aa50de01e4140ef
pbak95 <patryk.bak@gazeta.pl>
2017-06-06 10:48:00 +0200

    clear front and compile to prod, add links in new tab

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -4,16 +4,12 @@ import {RouterModule, Routes} from '@angular/router';
 import {LoginComponent}       from "./components/login/login.component";
 import {OrdersComponent}      from "./components/orders/orders.component";
 import {EBayComponent}        from "./components/eBay/eBay.component";
-import {HistoryComponent}     from "./components/history/history.component";
-import {SettingsComponent}    from "./components/settings/settings.component";
 import {ResultComponent} from "./components/results/result.component";
 
 const routes: Routes = [
   {path: '', redirectTo: '/login', pathMatch: 'full'},
   {path: 'orders', component: OrdersComponent},
   {path: 'eBay', component: EBayComponent},
-  {path: 'history', component: HistoryComponent},
-  {path: 'settings',component: SettingsComponent },
   {path: 'login', component: LoginComponent},
   {path: 'result', component: ResultComponent},
 ];
